7eeeb14c52c511fb054507412c805a5391deab4c
FajF3r <Macio.j2004@wp.pl>
2022-04-12 15:19:43 +0200

    functions for all checkboxes rectangle border (stroke and thickness)

--- praca/MainWindow.xaml.cs
+++ praca/MainWindow.xaml.cs
@@ -54,5 +54,67 @@ namespace praca
                 grayBox.Visibility = Visibility.Hidden;
             }
         }
+
+        private void blueCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (blueCheckBox.IsChecked == true)
+            {
+                blueBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void blueCheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            if (blueCheckBox.IsChecked == false)
+            {
+                blueBox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void greenCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (greenCheckBox.IsChecked == true)
+            {
+                greenBox.Visibility = Visibility.Visible;
+            }
+        }
+        private void greenCheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            if (greenCheckBox.IsChecked == false)
+            {
+                greenBox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void yellowCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (yellowCheckBox.IsChecked == true)
+            {
+                yellowBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void yellowCheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            if (yellowCheckBox.IsChecked == false)
+            {
+                yellowBox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void purpleCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (purpleCheckBox.IsChecked == true)
+            {
+                purpleBox.Visibility = Visibility.Visible;
+            }
+        }
+        private void purpleCheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            if (purpleCheckBox.IsChecked == false)
+            {
+                purpleBox.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
